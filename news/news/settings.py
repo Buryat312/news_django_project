7bb5e363c210a725df5661fdcb68c01f924d419e
Sultanbek <sultanbek.asakeev@gmail.com>
2021-07-29 20:59:20 +0600

    Set default auth user

--- news/news/settings.py
+++ news/news/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'posts',
     'tags',
     'comments',
+    'accounts',
 
 ]
 
@@ -104,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
